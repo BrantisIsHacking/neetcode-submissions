f204d0000cead99f08869d846cc8d32b3cd948c8
Brayden Uglione <brantisu5@gmail.com>
2026-08-13 21:15:49 -0400

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.py
@@ -0,0 +1,10 @@
+from collections import defaultdict
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        ana = defaultdict(list)
+
+        for str in strs:
+            ana[tuple(sorted(str))].append(str)
+        
+        return list(ana.values())
